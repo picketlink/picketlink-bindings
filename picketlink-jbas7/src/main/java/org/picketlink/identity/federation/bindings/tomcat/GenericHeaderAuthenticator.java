ca64bd7e97048160cfca2f8d7a67f83a7e570cbe
Anil Saldhana <Anil.Saldhana@jboss.com>
2013-11-07 17:32:53 -0600

    PLINK-295:GenericHeaderAuthenticator

---
+++ picketlink-jbas7/src/main/java/org/picketlink/identity/federation/bindings/tomcat/GenericHeaderAuthenticator.java
@@ -0,0 +1,44 @@
+/*
+ * JBoss, Home of Professional Open Source
+ *
+ * Copyright 2013 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.picketlink.identity.federation.bindings.tomcat;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.catalina.connector.Request;
+import org.apache.catalina.connector.Response;
+import org.apache.catalina.deploy.LoginConfig;
+
+/**
+ * JBAS-2283: Provide custom header based authentication support
+ *
+ * Header Authenticator that deals with userid from the request header Requires two attributes configured on the Tomcat Service
+ * - one for the http header denoting the authenticated identity and the other is the SESSION cookie
+ *
+ * @author Anil Saldhana
+ * @author Stefan Guilhen
+ * @version $Revision$
+ * @since Sep 11, 2006
+ */
+public class GenericHeaderAuthenticator extends AbstractGenericHeaderAuthenticator {
+
+    public boolean authenticate(Request request, HttpServletResponse response, LoginConfig config) throws IOException {
+        return super.performAuthentication(request, (Response) response, config);
+    }
+}
