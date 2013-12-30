5ec1e632af6f9058aca776fc5381c3d0965e03b8
Pedro Igor <pigor.craveiro@gmail.com>
2013-12-30 13:42:42 -0200

    Add ability to load roles from the authenticated Subject when the app is deployed in WildFly.

--- picketlink-wildfly-common/src/main/java/org/picketlink/identity/federation/bindings/wildfly/idp/UndertowRoleGenerator.java
+++ picketlink-wildfly-common/src/main/java/org/picketlink/identity/federation/bindings/wildfly/idp/UndertowRoleGenerator.java
@@ -17,27 +17,67 @@
  */
 package org.picketlink.identity.federation.bindings.wildfly.idp;
 
-import io.undertow.security.idm.Account;
+import org.jboss.security.SecurityContextAssociation;
 import org.picketlink.identity.federation.core.interfaces.RoleGenerator;
 
+import javax.security.auth.Subject;
 import java.security.Principal;
+import java.security.acl.Group;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.Set;
 
 /**
- * Implementation of {@link org.picketlink.identity.federation.core.interfaces.RoleGenerator}
- * for Undertow
+ * Implementation of {@link org.picketlink.identity.federation.core.interfaces.RoleGenerator} for Undertow
+ *
  * @author Anil Saldhana
  * @since December 06, 2013
  */
 public class UndertowRoleGenerator implements RoleGenerator {
-    protected List<String> roles = new ArrayList<String>();
+
     @Override
     public List<String> generateRoles(Principal principal) {
-        if(principal instanceof PicketLinkUndertowPrincipal){
+        if (principal instanceof PicketLinkUndertowPrincipal) {
             PicketLinkUndertowPrincipal pup = (PicketLinkUndertowPrincipal) principal;
-            return pup.getRoles();
+            return Collections.unmodifiableList(pup.getRoles());
+        } else {
+            return fromSubject();
         }
-        return roles;
+    }
+
+    /**
+     * <p>This method tries to load roles from the authenticated {@link javax.security.auth.Subject} obtained from
+     * {@link org.jboss.security.SecurityContextAssociation}.</p>
+     *
+     * <p>This method is particularly useful when the application is deployed in WildFly and the authentication is performed by
+     * a specific security domain (JAAS).</p>
+     *
+     * <p>Outside WildFly ecosystem, this method won't work as it relies on the security extension to get the subject.</p>
+     *
+     * @return
+     */
+    private List<String> fromSubject() {
+        List roles = new ArrayList();
+        Subject subject = SecurityContextAssociation.getSubject();
+
+        if (subject != null) {
+            Set<Group> groups = subject.getPrincipals(Group.class);
+
+            if (groups != null) {
+                for (Group group : groups) {
+                    if ("Roles".equals(group.getName())) {
+                        Enumeration<? extends Principal> subjectRoles = group.members();
+                        while (subjectRoles.hasMoreElements()) {
+                            Principal role = subjectRoles.nextElement();
+                            roles.add(role.getName());
+                        }
+                    }
+                }
+            }
+        }
+
+        return Collections.unmodifiableList(roles);
     }
 }
