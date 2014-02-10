905367aa05b7ea90164f1ed6bec0a08bebaa169d
Anil Saldhana <Anil.Saldhana@jboss.com>
2014-02-10 10:33:46 -0600

    PLINK-359: regular expression user name login module

---
+++ picketlink-jbas-common/src/main/java/org/picketlink/identity/federation/bindings/jboss/auth/RegExUserNameLoginModule.java
@@ -0,0 +1,121 @@
+/*
+ * JBoss, Home of Professional Open Source
+ *
+ * Copyright 2013 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.picketlink.identity.federation.bindings.jboss.auth;
+
+import java.security.Principal;
+import java.security.acl.Group;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.security.auth.login.LoginException;
+
+import org.jboss.security.ErrorCodes;
+import org.jboss.security.auth.spi.UsernamePasswordLoginModule;
+import org.picketlink.common.util.StringUtil;
+
+/**
+ * PLINK-359: Login Module that extracts user name from the principal based on a regular expression
+ * @author Anil Saldhana
+ * @since February 10, 2014
+ */
+public class RegExUserNameLoginModule extends UsernamePasswordLoginModule {
+
+    /** The login identity */
+    private Principal identity;
+    /** The proof of login identity */
+    private char[] credential;
+
+    @Override
+    public boolean login() throws LoginException {
+        // Setup our view of the user
+        Object username = sharedState.get("javax.security.auth.login.name");
+
+        if(username == null){
+            throw new LoginException(ErrorCodes.PROCESSING_FAILED  + ": No username");
+        }
+
+        if( username instanceof Principal){
+            identity = (Principal) username;
+
+            String extractedUserName = extractUserName(identity.getName());
+            try
+            {
+                identity = createIdentity(extractedUserName);
+            }
+            catch(Exception e)
+            {
+                log.debug("Failed to create principal", e);
+
+            }
+        }
+        else
+        {
+            String name = username.toString();
+
+            name = extractUserName(name);
+            try
+            {
+                identity = createIdentity(name);
+            }
+            catch(Exception e)
+            {
+                log.debug("Failed to create principal", e);
+                throw new LoginException(ErrorCodes.PROCESSING_FAILED  + "Failed to create principal: "+ e.getMessage());
+            }
+        }
+        Object password = sharedState.get("javax.security.auth.login.password");
+        if( password instanceof char[] )
+            credential = (char[]) password;
+        else if( password != null )
+        {
+            String tmp = password.toString();
+            credential = tmp.toCharArray();
+        }
+        // Add the principal and password to the shared state map
+        sharedState.put("javax.security.auth.login.name", identity);
+        sharedState.put("javax.security.auth.login.password", credential);
+
+        return true;
+    }
+
+    @Override
+    protected String getUsersPassword() throws LoginException {
+        return null;
+    }
+
+    @Override
+    protected Group[] getRoleSets() throws LoginException {
+        return new Group[0];
+    }
+
+    protected String extractUserName(String theName){
+        //The format is in the options
+        String regex = (String) options.get("regex");
+        if(StringUtil.isNotNull(regex)){
+            Pattern pattern = Pattern.compile(regex);
+
+            Matcher matcher = pattern.matcher(theName);
+
+            while(matcher.find()){
+                return matcher.group(1);
+            }
+        }
+
+        return theName;
+    }
+}
