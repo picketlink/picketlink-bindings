4bae700294eb05014b5dfedba8a3ff9b6f38617c
Pedro Igor <pigor.craveiro@gmail.com>
2014-03-07 13:29:28 -0300

    [PLINK-375] - Support SAMLConfigProvider and AuditHelper from WildFly IdP and SP bindings.

--- picketlink-wildfly-common/src/main/java/org/picketlink/identity/federation/bindings/wildfly/sp/SPServletExtension.java
+++ picketlink-wildfly-common/src/main/java/org/picketlink/identity/federation/bindings/wildfly/sp/SPServletExtension.java
@@ -33,6 +33,9 @@ import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
+import static org.picketlink.common.constants.GeneralConstants.AUDIT_HELPER;
+import static org.picketlink.common.constants.GeneralConstants.CONFIG_PROVIDER;
+
 /**
  *
  * <p>{@link io.undertow.servlet.ServletExtension} that enables the SAML authentication mechanism for service provider deployments.</p>
@@ -62,10 +65,40 @@ public class SPServletExtension implements ServletExtension {
             @Override
             public AuthenticationMechanism create(String mechanismName, FormParserFactory formParserFactory, Map<String, String> properties) {
                 SPFormAuthenticationMechanism authenticationMechanism = new SPFormAuthenticationMechanism(formParserFactory, mechanismName, properties
-                    .get(LOGIN_PAGE), properties.get(ERROR_PAGE), servletContext, configurationProvider, auditHelper);
+                    .get(LOGIN_PAGE), properties.get(ERROR_PAGE), servletContext, getConfigurationProvider(servletContext), getAuditHelper(servletContext));
 
                 return authenticationMechanism;
             }
         });
+    }
+
+    private SAMLConfigurationProvider getConfigurationProvider(ServletContext servletContext) {
+        String configProviderType = servletContext.getInitParameter(CONFIG_PROVIDER);
+
+        if (configProviderType != null) {
+            try {
+                return (SAMLConfigurationProvider) SecurityActions
+                    .loadClass(Thread.currentThread().getContextClassLoader(), configProviderType).newInstance();
+            } catch (Exception e) {
+                throw new RuntimeException("Could not create config provider [" + configProviderType + "].", e);
+            }
+        }
+
+        return this.configurationProvider;
+    }
+
+    private PicketLinkAuditHelper getAuditHelper(ServletContext servletContext) {
+        String auditHelperType = servletContext.getInitParameter(AUDIT_HELPER);
+
+        if (auditHelperType != null) {
+            try {
+                return (PicketLinkAuditHelper) SecurityActions
+                    .loadClass(Thread.currentThread().getContextClassLoader(), auditHelperType).newInstance();
+            } catch (Exception e) {
+                throw new RuntimeException("Could not create audit helper [" + auditHelperType + "].", e);
+            }
+        }
+
+        return this.auditHelper;
     }
 }
