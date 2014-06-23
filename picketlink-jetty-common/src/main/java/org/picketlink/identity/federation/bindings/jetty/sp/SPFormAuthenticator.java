1f271d5c48f7bfd2d5c0f65d7a6a1196c275ae08
Aaron Anderson <aanderson@cpsgpartners.com>
2014-06-23 00:39:14 -0500

    Corrected post SAML authentication Jetty authentication result

--- picketlink-jetty-common/src/main/java/org/picketlink/identity/federation/bindings/jetty/sp/SPFormAuthenticator.java
+++ picketlink-jetty-common/src/main/java/org/picketlink/identity/federation/bindings/jetty/sp/SPFormAuthenticator.java
@@ -298,7 +298,8 @@ public class SPFormAuthenticator extends FormAuthenticator {
             throws IOException, ServerAuthException {
         //only perform SAML Authentication if it is mandatory
         if(!mandatory){
-            return Authentication.UNAUTHENTICATED;
+            Request request = (Request) servletRequest;
+            return request.getAuthentication();
         }
         ServiceProviderSAMLWorkflow serviceProviderSAMLWorkflow = new ServiceProviderSAMLWorkflow();
         serviceProviderSAMLWorkflow.setRedirectionHandler(new JettyRedirectionHandler());
@@ -397,7 +398,7 @@ public class SPFormAuthenticator extends FormAuthenticator {
         session.setAttribute(FORM_ROLES_NOTE, roles);
         Request request = (Request) httpServletRequest;
         Authentication authentication = request.getAuthentication();
-        if (authentication == null) {
+        if (!(authentication instanceof UserAuthentication)) {
             Subject theSubject = new Subject();
             String[] theRoles = new String[roles.size()];
             roles.toArray(theRoles);
@@ -652,7 +653,7 @@ public class SPFormAuthenticator extends FormAuthenticator {
                     int redirectCode = (base_request.getHttpVersion().getVersion() < HttpVersion.HTTP_1_1.getVersion() ? HttpServletResponse.SC_MOVED_TEMPORARILY
                             : HttpServletResponse.SC_SEE_OTHER);
                     base_response.sendRedirect(redirectCode, response.encodeRedirectURL(nuri));
-                    return registeredAuthentication;
+                    return Authentication.SEND_SUCCESS; //since a redirect was made to the original requested URL inform Jetty the response has already been handled
 
                     // restoreRequest(request,session);
 
